bb51e516e0ce7c8f6c2e385e53c4325564329814
BoBrikkz <arsusha02@gmail.com>
2021-04-13 23:25:49 +0300

    feat: add autoprefixier

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,17 @@ gulp.task('browser-sync', function() {
         watch("./sass/**/*.sass").on('change', browserSync.reload);
         watch("./js/*.js").on('change', browserSync.reload);
 });
+gulp.task('autoprefixer', () => {
+  const autoprefixer = require('autoprefixer')
+  const sourcemaps = require('gulp-sourcemaps')
+  const postcss = require('gulp-postcss')
 
+  return gulp.src('./src/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dest'))
+})
 function serveSass() {
     return src("app/sass/*.sass")
         .pipe(sass())
