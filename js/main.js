a6833b29653c8ce77d4590ef91b9e32ba37ab0ad
BoBrikkz <arsusha02@gmail.com>
2021-04-17 00:12:33 +0300

    broken slider

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('.contacts__button');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-      modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('.button'),
+      closeBtn = $('.modal__close');
 
-  closeBtn.addEventListener('click', switchModal);
-  
+  modalBtn.on('click', function() {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function() {
+    modal.toggleClass('modal--visible');
+  });
+      $(document).keyup(function (event) {
+        if (event.key === "Escape") {
+            modal.removeClass('modal--visible');
+        }
+    });
+    $(document).click(function (event) {
+        if ($(event.target).is('.modal')) {
+            modal.toggleClass('modal--visible');
+        }
+    });
+});
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
 });
