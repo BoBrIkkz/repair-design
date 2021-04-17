ad7c4584f0a71606efe46bab1585d3d04b2a4c5e
BoBrikkz <arsusha02@gmail.com>
2021-04-17 23:17:58 +0300

    fix slider

--- js/main.js
+++ js/main.js
@@ -19,14 +19,21 @@ $(document).ready(function () {
             modal.toggleClass('modal--visible');
         }
     });
-});
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
+    const swiper = new Swiper('.swiper-container', {
   loop: true,
-
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
+    clickable: true,
   },
-
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+  next.css('left',prev.width() + 15 + bullets.width() + 15)
+  bullets.css('left', prev.width() + 15)
 });
