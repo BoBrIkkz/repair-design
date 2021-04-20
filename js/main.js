7fe5ea037bdd94a999c839dd26905a974c79d968
BoBrikkz <arsusha02@gmail.com>
2021-04-20 15:42:42 +0300

    add validate.js and mask.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var modal = $('.modal'),
-      modalBtn = $('.button'),
+      modalBtn = $('[data-toggle=modal]'),
       closeBtn = $('.modal__close');
 
   modalBtn.on('click', function() {
@@ -33,14 +33,51 @@ $(document).ready(function () {
 });
 
 
-
-
-
   var next = $('.swiper-button-next');
   var prev = $('.swiper-button-prev');
   var bullets = $('.swiper-pagination');
   next.css('left',prev.width() + 15 + bullets.width() + 15)
   bullets.css('left', prev.width() + 15)
   
+
+
   new WOW().init();
+
+  // Валидация
+
+  $('.modal__form').validate({
+    errorClass: "invalid",
+     rules: {
+    modalUserName: {
+      required: true,
+      minlength: 2,
+      maxlength: 15
+    },
+    modalUserPhone: {
+      required: true,
+    },
+    modalUserEmail: {
+      required: true,
+      email: true
+    }
+  },
+  messages: {
+    modalUserName: {
+      required: "Заполните поле",
+      minlength: "Имя не должно быть короче двух букв",
+      maxlength: "Имя не должно быть таким длинным"
+    },
+    modalUserPhone: {
+      required: "Заполните поле"
+    },
+    modalUserEmail: {
+      required: "Заполните поле",
+      email: "Введите корректный Email"
+    }
+  }
+  });
+
+  // Маски
+  $('[type=tel]').mask("+7 (000) 000-00-00", {placeholder: "+7 (999) 888-77-66"});
+
 });
