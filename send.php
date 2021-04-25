1c2d816ba492158f9e56c8ab9d692448a7d7389e
BoBrikkz <arsusha02@gmail.com>
2021-04-25 16:41:53 +0300

    add phpmailer and adaptive for mobile(576px)

---
+++ send.php
@@ -0,0 +1,39 @@
+<?php
+
+$modalUserName = $_POST['modalUserName'];
+$modalUserEmail = $_POST['modalUserEmail'];
+$modalUserPhone = $_POST['modalUserPhone'];
+
+// Load Composer's aUtoloader
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'FiilatovRDS@gmail.com';                     // SMTP username
+    $mail->Password   = 'FiilatovRDS1488';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('FiilatovRDS@gmail.com');
+    $mail->addAddress('arsusha202@gmail.com');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Имя пользователя: ${modalUserName}, Телефон: ${modalUserPhone}. Почта: ${modalUserEmail}";
+
+    $mail->send();
+    header('Location: thanks.html');    
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
